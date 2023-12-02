a0a6ec731866dbc0d6f395f3623c4dcdc4691721
Vaishnavi Farkande <121963995+VaishnaviF@users.noreply.github.com>
2023-12-03 00:10:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# AI_Virtual_Keyboard
+AI virtual keyboard</br>
+Required Modules:</br>
+-- opencv-python<br>
+-- cvzone==1.4.1</br>
+-- mediapipe</br>
+-- pynput</br>
---
+++ virtualKeyboard.py
@@ -0,0 +1,155 @@
+import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+import numpy as np
+from pynput.keyboard import Controller
+import mediapipe as mp
+
+
+
+class HandTracker():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLm in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLm, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def getPostion(self, img, handNo = 0, draw=True):
+        lmList =[]
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lmList.append([id, cx, cy])
+
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
+        return lmList
+
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.8)
+keyboard_keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+                  ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+                  ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
+
+final_text = ""
+
+keyboard = Controller()
+
+
+def draw(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cvzone.cornerRect(img, (button.pos[0], button.pos[1],
+                                                   button.size[0],button.size[0]), 20 ,rt=0)
+        cv2.rectangle(img, button.pos, (x + w, y + h),
+                      (255, 144, 30), cv2.FILLED)
+        cv2.putText(img, button.text, (x + 20, y + 65),
+                    cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+    return img
+
+
+def transparent_layout(img, buttonList):
+    imgNew = np.zeros_like(img, np.uint8)
+    for button in buttonList:
+        x, y = button.pos
+        cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1],
+                                                   button.size[0],button.size[0]), 20 ,rt=0)
+        cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]),
+                                   (255, 144, 30), cv2.FILLED)
+        cv2.putText(imgNew, button.text, (x + 20, y + 65),
+                    cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+
+    out = img.copy()
+    alpaha = 0.5
+    mask = imgNew.astype(bool)
+    print(mask.shape)
+    out[mask] = cv2.addWeighted(img, alpaha, imgNew, 1-alpaha, 0)[mask]
+    return out
+
+
+class Button():
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.size = size
+        self.text = text
+
+
+buttonList = []
+# mybutton = Button([100, 100], "Q")
+for k in range(len(keyboard_keys)):
+    for x, key in enumerate(keyboard_keys[k]):
+        buttonList.append(Button([100 * x + 25, 100 * k + 50], key))
+
+    while True:
+        s,img = cap.read()
+
+        hands = detector.findHands(img)
+
+
+        if hands:
+            hand = hands[0]  # Assuming you are interested in the first detected hand
+            # Handle the case when no hands are detected
+            lmList, bboxInfo = [], []
+            img = draw(img, buttonList)
+
+
+
+        # rest of the code...
+
+    if lmList:
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+
+            if x < lmList[8][0]<x+w and y < lmList[8][1] < y+h:
+                cv2.rectangle(img, button.pos, (x + w, y + h),
+                              (0, 255, 255), cv2.FILLED)
+                cv2.putText(img, button.text, (x + 20, y + 65),
+                            cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+                l, _, _ = detector.findDistance(8,12, img, draw=False)
+                print(l)
+
+                if l < 25:
+                    keyboard.press(button.text)
+                    cv2.rectangle(img, button.pos, (x + w, y + h),
+                                  (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, button.text, (x + 20, y + 65),
+                                cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+                    final_text += button.text
+                    sleep(0.20)
+
+    cv2.rectangle(img, (25,350), (700, 450),
+                  (255, 255, 255), cv2.FILLED)
+    cv2.putText(img, final_text, (60, 425),
+                cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+
+    # cv2.rectangle(img, (100,100), (200,200),
+    #               (100, 255, 0), cv2.FILLED)
+    # cv2.putText(img, 'Q', (120,180), cv2.FONT_HERSHEY_PLAIN, 5,
+    #             (0, 0, 0), 5)
+
+    # img = mybutton.draw(img)
+    cv2.imshow("output", img)
+    cv2.waitKey(1)
